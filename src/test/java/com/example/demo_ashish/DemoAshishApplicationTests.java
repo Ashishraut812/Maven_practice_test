6fa8438fa107935768c18f9022e500669594b9f0
Ashishraut812 <812ashishraut1999@gmail.com>
2025-04-19 16:43:44 +0530

    second commit

---
+++ src/test/java/com/example/demo_ashish/DemoAshishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_ashish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAshishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
